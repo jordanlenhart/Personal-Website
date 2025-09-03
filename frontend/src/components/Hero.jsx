e4c89ec26cd2a32b9a302ce9cb4a39960f0d9f74
Jordan <jordinholenhart@gmail.com>
2025-09-02 20:02:14 -0700

    Hero page mapped for mobile and added typewriter text.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,25 +1,39 @@
 import Kazuma from '../assets/Kazuma.png'
 import { FaLinkedin, FaGithub, FaDiscord, FaEnvelope } from "react-icons/fa";
+import Typewriter from './TypewriterTitle';
 
 export default function Hero() {
     return (
         <section
-            id="hero"
-            className="h-screen snap-center flex justify-center items-center text-center bg-[#0D1B2A] text-white gap-10"
-        >
-            <img
-                src={Kazuma}
-                alt="Jordan PFP"
-                className="w-[25em] h-[25em] rounded-4xl"
-            />
-            <div className="flex flex-col items-center gap-4 w-[75em]">
+    id="hero"
+    className="h-screen snap-center flex flex-col md:flex-row justify-center items-center text-center md:text-left bg-[#0D1B2A] text-white md:gap-50 px-4 md:px-20"
+>
+
+<img
+    src={Kazuma}
+    alt="Jordan PFP"
+    className="w-48 h-48 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-3xl object-cover"
+/>
+
+<div className="flex flex-col items-center md:items-center gap-4">
+
                 <h1 className="text-8xl font-bold mb-4">Jordan Lenhart</h1>
-                <p className="font-light text-3xl mb-6">
-                    Software Developer | Expert Google Searcher
-                </p>
+                <Typewriter
+                className="font-light text-xl sm:text-2xl md:text-3xl mb-4 md:mb-6 text-center md:text-left"
+                    texts={[
+                        "Software Developer",
+                        "Full Stack Developer",
+                        "Web Developer",
+                        "Pro Google Searcher",
+                        "React Enthusiast",
+                        "Merge Conflict Expert",
+                        '"Did you npm install yet"'
+
+                    ]}
+                />
                 <a
                     href="#projects"
-                    className="inline-block px-6 py-3 bg-blue-500 text-xl rounded-xl shadow hover:bg-blue-600"
+                    className="inline-block px-6 py-3 bg-blue-500 text-xl rounded-xl shadow hover:bg-blue-600 transition-all duration-300 transform hover:scale-115"
                 >
                     View My Work
                 </a>
@@ -30,7 +44,7 @@ export default function Hero() {
                         href="https://www.linkedin.com/in/jordanlenhart1/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="hover:text-blue-400 transition-colors"
+                        className="hover:text-blue-400 transition-all"
                     >
                         <FaLinkedin />
                     </a>
@@ -38,7 +52,7 @@ export default function Hero() {
                         href="https://github.com/jordanlenhart"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="hover:text-gray-400 transition-colors"
+                        className="hover:text-gray-400 transition-all"
                     >
                         <FaGithub />
                     </a>
@@ -46,13 +60,13 @@ export default function Hero() {
                         href="https://discordapp.com/users/bofa2x"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="hover:text-indigo-400 transition-colors"
+                        className="hover:text-indigo-400 transition-all"
                     >
                         <FaDiscord />
                     </a>
                     <a
                         href="#contact"
-                        className="hover:text-red-400 transition-colors"
+                        className="hover:text-red-400 transition-all"
                     >
                         <FaEnvelope />
                     </a>
